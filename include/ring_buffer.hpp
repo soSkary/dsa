3245a9ec915ba81009132b50e969f27db9787757
soSkary <oskar.siegfrids@gmail.com>
2024-03-05 23:12:18 +0200

    Container named requirements declaration done
     - Definitions still largely missing
     - Also other members missing

--- include/ring_buffer.hpp
+++ include/ring_buffer.hpp
@@ -2,6 +2,8 @@
 #define RING_BUFFER_HPP
 
 #include <iostream>
+#include <cstddef> // for std::ptrdiff_t, std::size_t
+#include <vector>
 
 namespace soskary
 {
@@ -9,18 +11,55 @@ namespace soskary
     class Ring_buffer
     {
     public:
+        using value_type = T;
+        using reference = T&;
+        using const_reference = const T&;
+        using iterator = T*;
+        using const_iterator = const T*;
+        using difference_type = std::ptrdiff_t;
+        using size_type = std::size_t;
+
         Ring_buffer();
+        Ring_buffer(const Ring_buffer<T>& other);
+        Ring_buffer(Ring_buffer<T>&& other);
+
+        Ring_buffer<T> operator=(const Ring_buffer<T>& other);
+        Ring_buffer<T> operator=(Ring_buffer<T>&& other);
+
+        void ~Ring_buffer();
+
+        iterator begin();
+        iterator end();
+        const_iterator cbegin();
+        const_iterator cend();
+        size_type size();
+        size_type max_size();
+        bool empty();
+
+
     private:
+        T* next_element_ptr;
+        T* last_element_ptr;
+        std::vector<T> container;
 
     };
 
+    template <typename T>
+    bool operator==(const Ring_buffer<T>& a, const Ring_buffer<T>& b)
+    {
 
+    }
 
     template <typename T>
-    Ring_buffer<T>::Ring_buffer()
+    bool operator!=(const Ring_buffer<T>& a, const Ring_buffer<T>& b)
     {
-        std::cout << "Created a Ring_buffer of type " << typeid(T).name() << '\n';
+        return !(a == b);
     }
+
+    template <typename T>
+    Ring_buffer<T>::Ring_buffer() :
+        container{ std::vector<T>(16) }, next_element_ptr{ container.data(); }, last_element_ptr{ container.data(); }
+    {}
 
 }
 #endif
